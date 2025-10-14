2ec897ee4cc14b1fe731f8ba705b7ce87409a966
tringuyen2020 <genhouchi@gmail.com>
2025-10-14 06:47:24 -0400

    Task 3: Format the output of the application

--- emotion_detection.py
+++ emotion_detection.py
@@ -9,12 +9,15 @@ def emotion_detector(text_to_analyze):
     response = requests.post(url, json=payload, headers=headers)
     if response.status_code == 200:
         json_data = response.json()
-        result = {'anger': json_data['emotionPredictions'][0]['emotion']['anger'],
-                'disgust': json_data['emotionPredictions'][0]['emotion']['disgust'],
-                'fear': json_data['emotionPredictions'][0]['emotion']['fear'],
-                'joy': json_data['emotionPredictions'][0]['emotion']['joy'],
-                'sadness': json_data['emotionPredictions'][0]['emotion']['sadness'],
-                'dominant_emotion': json_data['producerId']['name']
+        emotion = json_data['emotionPredictions'][0]['emotion']
+        dominant_emotion = max(emotion, key=emotion.get)
+
+        result = {'anger': emotion['anger'],
+                'disgust': emotion['disgust'],
+                'fear': emotion['fear'],
+                'joy': emotion['joy'],
+                'sadness': emotion['sadness'],
+                'dominant_emotion': dominant_emotion
                 }
         return json.dumps(result)
     else:
