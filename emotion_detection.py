adc4b5933bfa0e6348f5b5a0f32649d0781dfd31
tringuyen2020 <genhouchi@gmail.com>
2025-10-14 06:28:22 -0400

    format the output of the application

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -7,6 +8,14 @@ def emotion_detector(text_to_analyze):
 
     response = requests.post(url, json=payload, headers=headers)
     if response.status_code == 200:
-        return response.json()
+        json_data = response.json()
+        result = {'anger': json_data['emotionPredictions'][0]['emotion']['anger'],
+                'disgust': json_data['emotionPredictions'][0]['emotion']['disgust'],
+                'fear': json_data['emotionPredictions'][0]['emotion']['fear'],
+                'joy': json_data['emotionPredictions'][0]['emotion']['joy'],
+                'sadness': json_data['emotionPredictions'][0]['emotion']['sadness'],
+                'dominant_emotion': json_data['producerId']['name']
+                }
+        return json.dumps(result)
     else:
         return {"error": "Failed to get response from emotion detection service"}
