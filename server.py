347a03cf749abc42fce9cf9ab22b7eb02cc5f2ba
tringuyen2022 <genhouchi@gmail.com>
2025-10-15 02:40:50 -0400

    Task 6: Web deployment of the application using Flask

---
+++ server.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/emotionDetector", methods=['GET'])
+def emotionDetector():
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = json.loads(emotion_detector(text_to_analyze))
+
+    return f"For the given statement, the system response is 'anger': {response['anger']}, \
+        'disgust': {response['disgust']}, 'fear': {response['fear']}, 'joy': {response['joy']} \
+        and 'sadness': {response['sadness']}. \
+        The dominant emotion is <b>{response['dominant_emotion']}</b>."
+
+if __name__ == "__main__":
+    app.run(host="localhost", port="5000")
